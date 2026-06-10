dec465ae30cc4bdb57fcb8f0bfe9746cd95c0180
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-11 03:02:02 +0530

    files

---
+++ AI-Code-Review/backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CodeRequest(BaseModel):
+    code:str
+
+@app.post("/review")
+def review_code(data:CodeRequest):
+
+    return {
+        "received":data.code
+    }
