6de31792607403352f8c125501b30299942a708f
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-12 04:11:15 +0530

    frontend and file making

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -77,3 +77,26 @@
 
 //     document.getElementById("loading").style.display="none";
 // }
+async function reviewCode(){
+
+    const code =
+    document.getElementById("codeInput").value;
+
+    const response =
+    await fetch(
+        "http://localhost:8000/review",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({code})
+        }
+    );
+
+    const data =
+    await response.json();
+
+    document.getElementById("output")
+        .innerText = data.review;
+}
