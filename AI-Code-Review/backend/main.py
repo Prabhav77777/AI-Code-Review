65a54dfa2dc71a2dda9b6f344f1f2ee91ad9f7d2
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-12 04:13:41 +0530

    frontend files without ai inttegration

--- AI-Code-Review/backend/main.py
+++ AI-Code-Review/backend/main.py
@@ -1,12 +1,2 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-class CodeRequest(BaseModel):
-    code:str
-@app.post("/review")
-def review_code(data:CodeRequest):
-    return {
-        "received":len(data.code)
-    }
+def get_review(code):
+    return f"Your code contains {len(code)} characters"
---
+++ AI-Code-Review/backend/reviewer.py
@@ -0,0 +1,2 @@
+def get_review(code):
+    return f"Your code contains {len(code)} characters"
