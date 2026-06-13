3462c925c6e4523d1a27ef48936b6d265b7b1239
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-13 17:20:03 +0530

    removing monaco editor

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -1,55 +1,39 @@
-require.config({
-    paths:{
-        vs:"https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.2/min/vs"
+async function reviewCode() {
+
+    const code = document.getElementById("codeInput").value;
+
+    if (!code.trim()) {
+        alert("Please enter some code.");
+        return;
     }
-});
-
-require(["vs/editor/editor.main"], function(){
-
-    window.editor = monaco.editor.create(
-        document.getElementById("editor"),
-        {
-            value:`print("Hello World")`,
-            language:document.getElementById("language").value,
-            theme:"vs-dark",
-            automaticLayout:true,
-            minimap:{
-                enabled:false
-            }
-        }
-    );
-
-});
-
-async function reviewCode(){
 
     document.getElementById("loader").style.display = "block";
 
-    const code = editor.getValue();
-
-    try{
+    try {
 
         const response = await fetch(
             "http://localhost:8000/review",
             {
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                body:JSON.stringify({code})
+                body: JSON.stringify({
+                    code: code
+                })
             }
         );
 
         const data = await response.json();
 
         document.getElementById("output").innerText =
-        data.review;
+            data.review;
 
     }
-    catch(error){
+    catch (error) {
 
         document.getElementById("output").innerText =
-        "Error connecting to backend.";
+            "Error connecting to backend.";
 
         console.error(error);
     }
