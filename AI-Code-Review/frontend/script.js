9c013481bf9707d5c80dd16bfd7e154752ca63b4
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-13 14:53:55 +0530

    commit

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -1,26 +1,58 @@
-async function reviewCode(){
-    document.getElementById("loader").style.display = "block";
+require.config({
+    paths:{
+        vs:"https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.2/min/vs"
+    }
+});
 
-    const code =
-    document.getElementById("codeInput").value;
-    
-    const response =
-    await fetch(
-        "http://localhost:8000/review",
+require(["vs/editor/editor.main"], function(){
+
+    window.editor = monaco.editor.create(
+        document.getElementById("editor"),
         {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({code})
+            value:`print("Hello World")`,
+            language:document.getElementById("language").value,
+            theme:"vs-dark",
+            automaticLayout:true,
+            minimap:{
+                enabled:false
+            }
         }
     );
 
-    const data =
-    await response.json();
+});
+
+async function reviewCode(){
+
+    document.getElementById("loader").style.display = "block";
+
+    const code = editor.getValue();
+
+    try{
+
+        const response = await fetch(
+            "http://localhost:8000/review",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({code})
+            }
+        );
+
+        const data = await response.json();
+
+        document.getElementById("output").innerText =
+        data.review;
+
+    }
+    catch(error){
+
+        document.getElementById("output").innerText =
+        "Error connecting to backend.";
+
+        console.error(error);
+    }
+
     document.getElementById("loader").style.display = "none";
-
-    document.getElementById("output")
-        .innerText = data.review;
-
 }
