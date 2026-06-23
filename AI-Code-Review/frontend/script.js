80a97b44a62168079453909b9322a887036b407e
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-23 23:31:45 +0530

    correct

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -67,7 +67,7 @@ async function reviewCode() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("codeInput").style.border = "none";
     for (let i = 0; i < 3; i++) {
-        document.getElementsByClassName("review-box")[i].style.border = "2px solid white";
+        document.getElementsByClassName("review-box")[i].style.border = "2px solid var(--w)";
     }
 }
 
