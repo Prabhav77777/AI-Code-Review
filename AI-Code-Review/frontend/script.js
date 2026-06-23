39859f2046831599db09aae614c94c38b4eae73c
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-06-23 23:30:18 +0530

    border

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -102,7 +102,7 @@ function clearcode(){
     document.getElementById("issues").innerText = "";
     document.getElementById("suggestions").innerText = "";
     document.getElementById("optimizedCode").innerText = "";
-    document.getElementById("codeInput").style.border = "none";
+    document.getElementById("codeInput").style.border = "2px solid var(--w)";
     for (let i = 0; i < 3; i++) {
         document.getElementsByClassName("review-box")[i].style.border = "2px solid white";
     }
