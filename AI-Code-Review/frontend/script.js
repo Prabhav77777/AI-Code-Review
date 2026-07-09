33fad5fbaadb12c2990567b7455821cc8837abc4
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-07-09 22:32:26 +0530

    change

--- AI-Code-Review/frontend/script.js
+++ AI-Code-Review/frontend/script.js
@@ -69,9 +69,7 @@ async function reviewCode() {
     for (let i = 0; i < 3; i++) {
         document.getElementsByClassName("review-box")[i].style.border = "2px solid var(--w)";
     }
-    document.getElementById("loading").style.display = "flex";
-document.getElementById("review").disabled = true;
-}
+
 
 function updateList(id, items) {
 
